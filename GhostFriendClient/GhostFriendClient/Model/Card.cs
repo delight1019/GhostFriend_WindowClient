3d6e692fc1421650fc46f2436000c9c11c17c249
Hyehyeon-Kim <dev.hyehyeon.kim@gmail.com>
2020-04-03 21:39:52 +0900

    Create Card class

---
+++ GhostFriendClient/GhostFriendClient/Model/Card.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostFriendClient.Model
+{
+    public enum CardSuit
+    {
+        DIAMOND,
+        SPADE,
+        CLUB,
+        HEART,
+        JOKER
+    }
+    public enum CardValue
+    {
+        ACE,
+        TWO,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK,
+        QUEEN,
+        KING,
+        JOKER
+    }
+
+    public class Card
+    {
+        private CardSuit suit;
+        private CardValue value;        
+
+        public Card(String cardData)
+        {
+            String[] cardInfo = cardData.Split(' ');
+
+        }
+
+        public Card(CardSuit suit, CardValue value)
+        {
+            this.suit = suit;
+            this.value = value;
+        }
+    }
+}
