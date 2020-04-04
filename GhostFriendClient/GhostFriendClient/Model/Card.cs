df1ef5ba2312118be8b58d03209097dcf1cfc559
Hyehyeon-Kim <dev.hyehyeon.kim@gmail.com>
2020-04-04 22:18:31 +0900

    Create card converting function

--- GhostFriendClient/GhostFriendClient/Model/Card.cs
+++ GhostFriendClient/GhostFriendClient/Model/Card.cs
@@ -30,17 +30,75 @@ namespace GhostFriendClient.Model
         QUEEN,
         KING,
         JOKER
-    }
+    }    
 
     public class Card
     {
         private CardSuit suit;
         private CardValue value;        
 
+        private CardSuit convertCardSuit(String value)
+        {
+            switch (value)
+            {
+                case "DIAMOND":
+                    return CardSuit.DIAMOND;
+                case "SPADE":
+                    return CardSuit.SPADE;
+                case "CLUB":
+                    return CardSuit.CLUB;
+                case "HEART":
+                    return CardSuit.HEART;
+                case "JOKER":
+                    return CardSuit.JOKER;
+                default:
+                    return CardSuit.JOKER;
+            }
+
+        }
+        private CardValue convertCardValue(String value)
+        {
+            switch (value)
+            {
+                case "ACE":
+                    return CardValue.ACE;
+                case "TWO":
+                    return CardValue.TWO;
+                case "THREE":
+                    return CardValue.THREE;
+                case "FOUR":
+                    return CardValue.FOUR;
+                case "FIVE":
+                    return CardValue.FIVE;
+                case "SIX":
+                    return CardValue.SIX;
+                case "SEVEN":
+                    return CardValue.SEVEN;
+                case "EIGHT":
+                    return CardValue.EIGHT;
+                case "NINE":
+                    return CardValue.NINE;
+                case "TEN":
+                    return CardValue.TEN;
+                case "JACK":
+                    return CardValue.JACK;
+                case "QUEEN":
+                    return CardValue.QUEEN;
+                case "KING":
+                    return CardValue.KING;
+                case "JOKER":
+                    return CardValue.JOKER;
+                default:
+                    return CardValue.JOKER;
+            }
+        }
+
         public Card(String cardData)
         {
             String[] cardInfo = cardData.Split(' ');
 
+            this.suit = convertCardSuit(cardInfo[0]);
+            this.value = convertCardValue(cardInfo[1]);
         }
 
         public Card(CardSuit suit, CardValue value)
