9272dfe836d878d429a2954636794dce4a503d57
Hyehyeon-Kim <dev.hyehyeon.kim@gmail.com>
2020-04-09 22:30:54 +0900

    Make card list UI

--- GhostFriendClient/GhostFriendClient/Converters/CardConverter.cs
+++ GhostFriendClient/GhostFriendClient/Converters/CardConverter.cs
@@ -13,13 +13,27 @@ namespace GhostFriendClient.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            CardValue cardSuit = (CardValue)value;
-            return cardSuit.ToString();
+            CardValue cardValue = (CardValue)value;
+            return cardValue.ToString();
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return Card.ConvertCardValue(value as string);
+        }
+    }
+
+    public class CardSuitConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            CardSuit cardSuit = (CardSuit)value;
+            return cardSuit.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Card.ConvertCardSuit(value as string);
         }
     }
 }
